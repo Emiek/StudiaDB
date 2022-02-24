e0ed748e6e073211eb5bf7235db095da8d173928
PatrykBachanek <92025612+PatrykBachanek@users.noreply.github.com>
2022-02-24 10:28:12 +0100

    Add files via upload

---
+++ cw.py
@@ -0,0 +1,18 @@
+def mysum(a,b):
+    """to jest komentarz"""
+    return a+b
+def plus42(n):
+    l=n+42
+    return l
+def return42():
+    return 42
+def print42():
+    print(42)
+print('Suma 3 i 4 to', mysum(3,4))
+x= -1.5
+y=abs(x)
+print(y)
+a=8
+b=plus42(a)
+print(b)
+return42()
---
+++ cw2.py
@@ -0,0 +1,7 @@
+import math as m
+# mozna tez import math as m
+# mozemy wyciagnosc konkretne funkcje np.
+#from math import sin, pi
+# from math import * nie polecane
+print(m.sqrt(4))
+print(m.pi)
